dc15b91097f0dbc825024caa390b83cc833d7a02
NurGeo <anzpro@gmail.com>
2024-04-01 09:17:23 +0500

    мелкие правки

--- src/subject/domain-data/user/refresh/a-params.ts
+++ src/subject/domain-data/user/refresh/a-params.ts
@@ -1,18 +1,12 @@
 import { JwtVerifyErrors } from 'rilata/src/app/jwt/jwt-errors';
-import { ErrorDod } from 'rilata/src/domain/domain-data/domain-types';
 import { ActionParams } from 'rilata/src/domain/domain-data/params-types';
+import { RefreshTokenIsExpiredError } from '../aggregate-errors';
 
 export type UserRefreshDomainQuery = {
   refreshToken: string,
 };
 
 export type UserRefreshDomainOut = string; // access token
-
-export type RefreshTokenIsExpiredError = ErrorDod<'RefreshTokenIsExpiredError', {
-  name: 'RefreshTokenIsExpiredError',
-  text: 'Время рефреш токена истекло',
-  hint: Record<never, unknown>
-}>
 
 export type UserRefreshActionParams = ActionParams<
   UserRefreshDomainQuery,
